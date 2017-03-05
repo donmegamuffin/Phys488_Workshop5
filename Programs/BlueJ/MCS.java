3a198cb5ca962ca6d8c04d2c8cc6355ae34e956f
donmegamuffin <uol.zhumphreys@outlook.com>
2017-03-05 04:10:04 +0000

    MCS/Task1 complete
    Commented and added Task1 which gives EXACTLY the request from the
    report with correct significant figures.
    MCS updated to take thickness argument in correct place.

--- Programs/BlueJ/MCS.java
+++ Programs/BlueJ/MCS.java
@@ -2,9 +2,8 @@
 *   Intructions: 
 *   2 Sets of inputs are required for all of the code to function.
 *   1)The constructor requires 4 material arguments (in order): atomic number, atomic mass, density, material thickness
-*   2)The theta/sigma methods require addition particle argument: momentum (MeV)
+*   2)The theta/sigma methods require addition particle arguments: particle momentum (MeV), and material thickness (cm)
 */
-
 import java.io.*;
 
 class MCS
@@ -17,31 +16,29 @@ class MCS
     private double material_density;                        //gcm^{-3}
     private int    material_Z;
     private double material_A;
-    private double material_thickness;                      //cm
     //Particle variables: Taken as constants
     private double particle_charge = 1;                     //Coulombs
     private double particle_mass = 106;                     //MeV
     
     //Constructor
-    public MCS(int in_Z, double in_A, double in_density, double in_thickness)
+    public MCS(int in_Z, double in_A, double in_density)
     {
         material_density = in_density;
         material_Z = in_Z;
         material_A = in_A;
-        material_thickness = in_thickness;
     }
     
     //Calculates the material radiation length
-    public double X_0()
+    public double getX_0()
     {
         double X_0 = ((716.4*material_A)/(material_density*material_Z*(material_Z+1)*Math.log(287/Math.sqrt(material_Z))));
         return X_0;
     }
     
     //Calculates the theta_0 angle of scattering
-    public double theta_0(double particle_momentum)
+    public double getTheta0(double particle_momentum, double material_thickness)
     {
-        double x_ratio = material_thickness/X_0();
+        double x_ratio = material_thickness/getX_0();
         //Calculates the velocity as speed of light fractions
         double particle_beta = (particle_momentum/Math.sqrt((Math.pow(particle_momentum,2)+Math.pow(particle_mass,2))));
         double theta_0 = ((Rydberg_constant*particle_charge*Math.sqrt(x_ratio))*(1+0.038*Math.log(x_ratio))/(particle_beta*particle_momentum));
@@ -49,9 +46,9 @@ class MCS
     }
 
     //Calculates the theta_t angle of scattering
-    public double findSigma(double particle_momentum)
+    public double getThetaT(double particle_momentum, double material_thickness)
     {
-        double theta_t = (Math.sqrt(2)*theta_0(particle_momentum));
+        double theta_t = (Math.sqrt(2)*getTheta0(particle_momentum, material_thickness));
         return theta_t;
     }
 }
---
+++ Programs/BlueJ/Task1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+class Task1
+{   //Data values for task question part 1, and part 2
+    static double[] p1_array = {30,300,3000,10000,30000,100000};   //stores part1 momenta
+    static double[] p2_array = {500,1000,3000};                    //stores part2 momenta
+    //Initialise Materials IRON:
+    static EnergyLoss ironEloss = new EnergyLoss(26,55.845,7.87);
+    static MCS ironMS = new MCS(26,55.845,7.87);
+    static double iron_thickness = 1;
+    /*COPPER:
+    EnergyLoss copperEloss = new EnergyLoss(29,63.546,8.96);
+    MCS copperMS = new MCS(29,63.546,8.96);
+    */
+    public static void main()
+    {
+        System.out.println("\nThe values for energy loss:");
+        //Cycles through until it reaches the end of the array of momentums
+        for(int n=0;n<p1_array.length;n++)
+        {   //This prints out the output wanted from part 1 cycling through the array values to console
+            System.out.printf("dE/dx = %.2f",ironEloss.getEnergyLoss(p1_array[n])); //For 2 decimal points
+            System.out.println(" MeV/cm for p = "+p1_array[n]+" MeV");
+        }
+        System.out.println("\nThe values for MCS:");
+        System.out.printf("Iron X0 = %.3f",ironMS.getX_0());
+        System.out.println("cm\nFor a material thickness of 1cm:");
+        for(int n=0;n<p2_array.length;n++)
+        {   //This prints out the output wanted from part 2 cycling through the array values to console (5 sigfig)
+            System.out.printf("thetaT = %.5f",ironMS.getThetaT(p2_array[n],iron_thickness));
+            System.out.println(" for p = "+p2_array[n]+" MeV");
+        }
+    }   
+}
